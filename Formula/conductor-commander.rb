5f25c4d5fdcbede931cc45314ea11d0e8a5b4486
my1795 <myldrm1025@gmail.com>
2023-09-02 12:43:14 +0300

    conductor-commander v1.0.2

--- Formula/conductor-commander.rb
+++ Formula/conductor-commander.rb
@@ -1,20 +1,20 @@
-# Generated with JReleaser 1.7.0 at 2023-08-22T23:07:44.121353+03:00
+# Generated with JReleaser 1.7.0 at 2023-09-02T12:43:12.191738+03:00
 class ConductorCommander < Formula
   desc "Netflix Conductor CLi"
   homepage "https://github.com/my1795/conductor-commander"
-  url "https://github.com/my1795/conductor-commander/releases/download/v1.0.1/conductor-commander-1.0.1-osx-aarch_64.zip"
-  version "1.0.1"
-  sha256 "cf12a8256a1ffc58ccfd152283db817cd16bef2372bd6218851af2596c876d71"
+  url "https://github.com/my1795/conductor-commander/releases/download/v1.0.2/conductor-commander-1.0.2-osx-aarch_64.zip"
+  version "1.0.2"
+  sha256 "c17eb44b2abfcdaea9739d9812b88e5b5abe339c9e6d4595aff2ee8b9b355e38"
   license "Apache-2.0"
 
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/cnd" => "cnd"
+    bin.install_symlink "#{libexec}/bin/cnd.sh" => "cnd"
   end
 
   test do
     output = shell_output("#{bin}/cnd --version")
-    assert_match "conductor-commander 1.0", output
+    assert_match "1.0.2", output
   end
 end
