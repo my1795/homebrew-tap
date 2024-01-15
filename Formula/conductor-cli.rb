ca24175a8f2e7a24a470aa823cc42c6570617183
Mustafa Yıldırım <myldrm1025@gmail.com>
2024-01-15 13:38:08 +0300

    updated tag

--- Formula/conductor-cli.rb
+++ Formula/conductor-cli.rb
@@ -2,8 +2,8 @@ class ConductorCli < Formula
   desc "Cli app for conductor oss https://conductor-oss.org"
   homepage "https://github.com/my1795/conductor-cli"
   url "https://github.com/my1795/conductor-cli.git",
-      tag:      "v1.0.5",
-      revision: "d03c3f2b17c97505a6f6211b32eacb2669e1af1b"
+      tag:      "v1.0.6",
+      revision: "e2a4e41851d37ae4ecc203ebe66ced0d295acd69"
   license "Apache-2.0"
   head "https://github.com/my1795/conductor-cli.git", branch: "main"
   depends_on "go" => :build
